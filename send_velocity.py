0a71bee8880db116110eab44c1e3d49ae1643718
ADITYA SINGH RAWAT <adityasrwt5@gmail.com>
2021-05-21 19:54:12 +0530

    send velocity sample

--- send_velocity.py
+++ send_velocity.py
@@ -13,6 +13,7 @@ def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
         0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
 
 
+
     # send command to vehicle on 1 Hz cycle
     for x in range(0,duration):
         vehicle.send_mavlink(msg)
